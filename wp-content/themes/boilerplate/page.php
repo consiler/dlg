ff2b196f84a827a060979766c2fe67e71d9efa9a
MP <consiler@gmail.com>
2013-10-10 20:10:18 -0400

    more css, added images on internal page template

--- wp-content/themes/boilerplate/page.php
+++ wp-content/themes/boilerplate/page.php
@@ -13,36 +13,45 @@
  */
 
 get_header(); ?>
-
-<div id="internal-main">
-  <header id="internal-main-header">
-    <h3>It's not what we do, but how we do it, that delivers results.</h3>
-    <p>We work as an extension of our clients...</p>
-  </header>
-  <div id="internal-main-body">
-    <nav id="internal-main-nav">
-    </nav>
-    <div id="internal-main-content">
-      <article class="blah">
-      </article>
-      <?php //OR ?>
-      <h4>Something important</h4>
-      <p>Something about that important thing</p>
+<div id="internal-wrap">
+  <div id="internal-header-wrap">
+    <div id="internal-header" class="centered">
+      <header id="internal-header-text">
+        <h3>It's not what we do, but how we do it, that delivers results.</h3>
+        <p>We work as an extension of our clients...</p>
+      </header>
+      <div id="internal-banner-image">
+        <img src="<?php bloginfo('template_url'); ?>/images/stock-man-working.jpg">
+      </div>
+    </div>
+  </div>
+  <div id="internal-lower-wrap">
+    <div id="internal-lower" class="centered">
+      <div id="internal-main">
+        <div id="internal-main-body">
+          <nav id="internal-main-nav">
+          </nav>
+          <div id="internal-main-content">
+            <article class="blah">
+            </article>
+            <?php //OR ?>
+            <h4>Something important</h4>
+            <p>Something about that important thing</p>
+          </div>
+        </div>
+      </div>
+      <aside id="internal-sidebar">
+        <?php //the following divs can be styled as is, but we should make them into wordpress widgets or custom post types ?>
+        <div id="internal-sidebar-detour-link">
+          <h4>Dell partnered with Dialog...</h4>
+          <button class="light-grey-button">Read the case study</button>
+        </div>
+        <div id="internal-sidebar-decoration">
+          <?php //put a fancy geometry image or something here ?>
+        </div>
+      </aside>
     </div>
   </div>
 </div>
-<aside id="internal-sidebar">
-  <div id="internal-banner-image">
-    <img src="<?php //dynamically load this from a custom field called banner image ?>">
-  </div>
-  <?php //the following divs can be styled as is, but we should make them into wordpress widgets or custom post types ?>
-  <div class="internal-sidebar-detour-link">
-    <h4>Dell partnered with Dialog...</h4>
-    <button class="light-grey-button">Read the case study</button>
-  </div>
-  <div class="internal-sidebar-decoration">
-    <?php //put a fancy geometry image or something here ?>
-  </div>
-</aside>
 
 <?php get_footer(); ?>
