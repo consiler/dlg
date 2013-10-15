35b5047b624f722fb7756af58f338ceeaae3ebd6
sanford78 <sanford78@gmail.com>
2013-10-15 00:26:32 -0400

    Updated columns on front page

--- wp-content/themes/boilerplate/front-page.php
+++ wp-content/themes/boilerplate/front-page.php
@@ -61,14 +61,42 @@ get_header(); ?>
 <div id="fp-testimonials-wrap">
   <div id="fp-testimonials" class="centered">
     <div id="fp-testimonials-seemore">
-      <p>We have worked with a range of clients from startups to Fortune 500 companies to help them solve a wide range of problems.
-      </p>
-      <a href="/testimonials"><span class="light-grey-button">Full Testimonials</span></a>
+      <?php
+      $header = get_field('header_1');
+      $copy = get_field('copy_1');
+      $cta = get_field('cta_1');
+      $cta_link = get_field('cta_link_1');
+      ?>
+      <h4><?php echo $header ?></h4>
+      <p><?php echo $copy ?></p>
+      <a href="<?php echo $cta_link ?>"><span class="light-grey-button"><?php echo $cta ?></span></a>
     </div>
-    <div id="fp-testimonials-logos">
+    <div id="fp-testimonials-seemore">
+      <?php
+      $header = get_field('header_2');
+      $copy = get_field('copy_2');
+      $cta = get_field('cta_2');
+      $cta_link = get_field('cta_link_2');
+      ?>
+      <h4><?php echo $header ?></h4>
+      <p><?php echo $copy ?></p>
+      <a href="<?php echo $cta_link ?>"><span class="light-grey-button"><?php echo $cta ?></span></a>
+    </div>
+    <div id="fp-testimonials-seemore">
+      <?php
+      $header = get_field('header_3');
+      $copy = get_field('copy_3');
+      $cta = get_field('cta_3');
+      $cta_link = get_field('cta_link_3');
+      ?>
+      <h4><?php echo $header ?></h4>
+      <p><?php echo $copy ?></p>
+      <a href="<?php echo $cta_link ?>"><span class="light-grey-button"><?php echo $cta ?></span></a>
+    </div>
+    <!-- <div id="fp-testimonials-logos">
       <img src="<?php bloginfo('template_url'); ?>/images/testimonials-client-logos.jpg">
       <button><?php //arrow button, may indicate that this is a slideshow of images with logos ?></button>
-    </div>
+    </div> -->
   </div>
 </div>
 
