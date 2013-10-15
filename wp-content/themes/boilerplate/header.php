7565eb9543f29141b49700581950e50e4e3b36ec
MP <consiler@gmail.com>
2013-10-15 04:08:03 -0400

    finished navbar

--- wp-content/themes/boilerplate/header.php
+++ wp-content/themes/boilerplate/header.php
@@ -40,68 +40,7 @@ valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
     <script src="<?php bloginfo('template_url'); ?>/js/orbit/jquery.orbit.js" type="text/javascript"></script>
 		<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/js/orbit/orbit.css">
-    <script>
-    //Navbar code
-      $(document).ready(function(){
-        var slideMenu = $('#nav-menu-slide');
-
-        //Extract the sub menus
-        $('#menu-top-navigation > li > .sub-menu').each(function(i,v){
-          var vj = $(v);
-          var slideCenter = $('#nav-menu-slide-center');
-          var group_index = 0;
-          //Partition the submenu into groups of 3 or less
-          while(vj.children('li').length > 3){
-            //get 3 children and allocate them into a new submenu
-            vj.children("li:lt(3)").wrapAll("<div></div>");
-            group_index++;
-            if(group_index > 100) break;
-          }
-          vj.children('li').wrapAll("<div>");
-          //move the submenu to slide menu
-          var parent = vj.parent();
-          var parent_position_left = parent.position().left;
-          var parent_index = parent.index();
-          parent.addClass('hassubmenu');
-          vj.css({left: parent_position_left});
-          slideCenter.append(vj.attr('data-sub-menu-index',''+parent_index));
-        });
-
-        //Manage slide menu hover state
-        $('#nav-menu-slide').hover(function(){
-          slideMenu.stop();
-        }, function(){
-          slideMenu.stop()
-                .animate({
-                  top: -24
-                }, 400, 'swing', function(){
-                  slideMenu.stop();
-              });
-        });
-
-        //Activate and close slide menu hover state from top level
-        $('#menu-top-navigation > li').each(function(i,v){
-          if($(v).hasClass('hassubmenu')){
-            $(v).hover(function() {
-              $('#nav-menu-slide-center ul').hide();
-              $('#nav-menu-slide-center ul[data-sub-menu-index='+i+']').css({display: 'block'});
-              slideMenu.stop()
-              .animate({
-                top: 71
-              });
-            }, function(){
-              slideMenu.stop()
-                .animate({
-                  top: -24
-                }, 400, 'swing', function(){
-                  $('#nav-menu-slide-center ul[data-sub-menu-index='+i+']').hide();
-                  slideMenu.stop();
-              });
-            });
-          }
-        });
-      });
-    </script>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/navbar.js"></script>
 <?php
 		//If you delete this line, the wordpress admin menu will not appear at the top of the page
 		wp_head();
---
+++ wp-content/themes/boilerplate/js/navbar.js
@@ -0,0 +1,60 @@
+//Navbar code
+$(document).ready(function(){
+  var slideMenu = $('#nav-menu-slide');
+
+  //Extract the sub menus
+  $('#menu-top-navigation > li > .sub-menu').each(function(i,v){
+    var vj = $(v);
+    var slideCenter = $('#nav-menu-slide-center');
+    var group_index = 0;
+    //Partition the submenu into groups of 3 or less
+    while(vj.children('li').length > 3){
+      //get 3 children and allocate them into a new submenu
+      vj.children("li:lt(4)").wrapAll("<div></div>");
+      group_index++;
+      if(group_index > 100) break;
+    }
+    vj.children('li').wrapAll("<div>");
+    //move the submenu to slide menu
+    var parent = vj.parent();
+    var parent_position_left = parent.position().left;
+    var parent_index = parent.index();
+    parent.addClass('hassubmenu');
+    vj.css({left: parent_position_left});
+    slideCenter.append(vj.attr('data-sub-menu-index',''+parent_index));
+  });
+
+  //Manage slide menu hover state
+  $('#nav-menu-slide').hover(function(){
+    slideMenu.stop();
+  }, function(){
+    slideMenu.stop()
+          .animate({
+            top: -24
+          }, 400, 'swing', function(){
+            slideMenu.stop();
+        });
+  });
+
+  //Activate and close slide menu hover state from top level
+  $('#menu-top-navigation > li').each(function(i,v){
+    if($(v).hasClass('hassubmenu')){
+      $(v).hover(function() {
+        $('#nav-menu-slide-center ul').hide();
+        $('#nav-menu-slide-center ul[data-sub-menu-index='+i+']').css({display: 'block'});
+        slideMenu.stop()
+        .animate({
+          top: 99
+        });
+      }, function(){
+        slideMenu.stop()
+          .animate({
+            top: -24
+          }, 400, 'swing', function(){
+            $('#nav-menu-slide-center ul[data-sub-menu-index='+i+']').hide();
+            slideMenu.stop();
+        });
+      });
+    }
+  });
+});
